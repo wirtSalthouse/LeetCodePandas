d72b5f5dba9aced027bdb965860b8b83b93c01b1
Wirt4 <wirt.salthouse@gmail.com>
2023-09-04 13:07:26 -0700

    let some air out

--- InvalidTweets/invalidTweets.py
+++ InvalidTweets/invalidTweets.py
@@ -2,10 +2,14 @@ import pandas as pd
 
 
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    char_limit = 15
     if tweets.empty:
-        return pd.DataFrame({'tweet_id': []})
+        return return_df([])
 
     tweets.content = tweets.content.astype('str')
-    applied_mask = tweets.content.apply(lambda x: len(x) > 15)
-    filtered = tweets.loc[applied_mask]
-    return pd.DataFrame({'tweet_id': filtered['tweet_id'].values.tolist()})
+    filtered = tweets.loc[tweets.content.apply(lambda x: len(x) > char_limit)]
+    return return_df(filtered.tweet_id.tolist())
+
+
+def return_df(data_list):
+    return pd.DataFrame({'tweet_id': data_list})
