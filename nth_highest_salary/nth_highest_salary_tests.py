805602d1c319ae88e10301dc1f68fd7facf82f30
Wirt4 <wirt.salthouse@gmail.com>
2023-09-04 15:32:14 -0700

    initialized project proper with readme

---
+++ nth_highest_salary/nth_highest_salary_tests.py
@@ -0,0 +1,11 @@
+import unittest
+import pandas as pd
+
+# remember to import the function to test
+
+class nth_highest_salaryTests(unittest.TestCase):
+	def test_something(self):
+		self.assertEqual(True, False)
+
+if __name__ == '__main__':
+	unittest.main()
