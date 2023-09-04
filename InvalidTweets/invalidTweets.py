ff4f6dd73759743587a3de6ccb7e7f0c3ecd5132
Wirt4 <wirt.salthouse@gmail.com>
2023-09-04 13:04:15 -0700

    passes tests

--- InvalidTweets/invalidTweets.py
+++ InvalidTweets/invalidTweets.py
@@ -4,5 +4,8 @@ import pandas as pd
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
     if tweets.empty:
         return pd.DataFrame({'tweet_id': []})
-    filtered = tweets.query('content == \'Let us make America great again!\'')
+
+    tweets.content = tweets.content.astype('str')
+    applied_mask = tweets.content.apply(lambda x: len(x) > 15)
+    filtered = tweets.loc[applied_mask]
     return pd.DataFrame({'tweet_id': filtered['tweet_id'].values.tolist()})
--- InvalidTweets/invalidTweetsTests.py
+++ InvalidTweets/invalidTweetsTests.py
@@ -30,6 +30,16 @@ class MyTestCase(unittest.TestCase):
         result = invalid_tweets(input_table)
         self.assertEqual(is_equal_dataframes(result, desired_output), True)
 
+    def test_different_overload_data(self):
+        preamble = 'When in the Course of human events, it becomes necessary for one people to dissolve the political ' \
+                   'bands which have connected them with another, and to assume among the powers of the earth, ' \
+                   'the separate and equal station to which the Laws of Nature and of Nature\'s God entitle them, ' \
+                   'a decent respect to the opinions of mankind requires that they should declare the causes which ' \
+                   'impel them to the separation. '
+        input_table = self.input_df([[1776, preamble]])
+        desired_output = self.output_df([1776])
+        result = invalid_tweets(input_table)
+        self.assertEqual(is_equal_dataframes(result, desired_output), True)
 
 if __name__ == '__main__':
     unittest.main()
