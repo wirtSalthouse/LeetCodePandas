303c7957efc7884aa0d2ae04b1bb5d27c40b6f5f
Wirt4 <wirt.salthouse@gmail.com>
2023-09-03 19:47:17 -0700

    refactor solution to use a class to DRY up static values and make code cleaner

--- BigCountries/bigCountries.py
+++ BigCountries/bigCountries.py
@@ -4,16 +4,25 @@ import pandas as pd
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     """
     returns a pandas DataFrame containing only "big countries",
-    table entries which have a area greater than 3,000,000 or population greater than 25,000,000
-    :param world:
-    :return:new dataframe with schema of name, population and area
-    """
+    table entries which have an area greater than 3,000,000 or a population greater than 25,000,000
+    :param world: DataFrame with keys including name, population, and area
+    :return:new DataFrame with schema of name, population, and area
+    """,
 
-    min_area = 3000000
-    min_population = 25000000
+    country_parser = CountryParser()
+    selection = country_parser.get_selection(world)
+    output_data = selection.values.tolist()
+    return country_parser.new_dataframe(output_data)
 
-    query_string = f'(area >= {min_area}) | (population >= {min_population})'
-    output_schema = ['name', 'population', 'area']
-    output_data = world.query(query_string)[output_schema].values.tolist()
-    #whole new df to match indexing for test cases
-    return pd.DataFrame(output_data, columns=output_schema)
+
+class CountryParser():
+    _area = 3000000
+    _pop = 25000000
+    _schema = ['name', 'population', 'area']
+
+    def get_selection(self, dataframe):
+        query_string = f'(area >= {self._area}) | (population >= {self._pop})'
+        return dataframe.query(query_string)[self._schema]
+
+    def new_dataframe(self, data):
+        return pd.DataFrame(data, columns=self._schema)
--- BigCountries/test.py
+++ BigCountries/test.py
@@ -2,8 +2,8 @@ import unittest
 import pandas as pd
 from bigCountries import big_countries
 
-def df_from_schema_and_data(scehma, data):
-    return pd.DataFrame(data,columns=scehma)
+def df_from_schema_and_data(schema, data):
+    return pd.DataFrame(data, columns=schema)
 
 def input_dataframe(data_list):
     return df_from_schema_and_data(['name', 'continent', 'area', 'population', 'gdp'], data_list)
@@ -22,13 +22,14 @@ class BigCountriesTest(unittest.TestCase):
                       ['Andorra', 'Europe', 468, 78115, 3712000000],
                       ['Angola', 'Africa', 1246700, 20609294, 100990000000]]
 
-        desired_data = [['Afghanistan', 25500100, 652230],
+        desired_output_data = [['Afghanistan', 25500100, 652230],
                        ['Algeria', 37100000, 2381741]]
 
-        desired_result = output_dataframe(desired_data)
         input_df = input_dataframe(input_data)
+
         result = big_countries(input_df)
 
+        desired_result = output_dataframe(desired_output_data)
         self.assert_equal_dataframes(result, desired_result)
 
     def test_case_2(self):
@@ -37,21 +38,37 @@ class BigCountriesTest(unittest.TestCase):
                       ['Andorra', 'Europe', 468, 78115, 3712000000],
                       ['Angola', 'Africa', 1246700, 20609294, 100990000000]]
 
-        output_data = [['Afghanistan', 25500100, 652230]]
+        desired_output_data = [['Afghanistan', 25500100, 652230]]
 
         input_df = input_dataframe(input_data)
-        output_df = output_dataframe(output_data)
-        self.assert_equal_dataframes(big_countries(input_df), output_df)
+
+        result = big_countries(input_df)
+
+        desired_result = output_dataframe(desired_output_data)
+        self.assert_equal_dataframes(result, desired_result)
 
     def single_entry_different_data(self):
         input_data = [['Latveria', 'Europe', 600, 1000000],
                       ['Wakanda', 'Africa', 28748, 25000000, 1897900000],
                       ['Duckburg', 'Calisota', 10000, 78115, 261200000000000000000]]
 
-        output_data = [['Wakanda', 25000000, 28748]]
+        desired_output_data = [['Wakanda', 25000000, 28748]]
 
         input_df = input_dataframe(input_data)
-        desired_df = output_dataframe(output_data)
+        desired_df = output_dataframe(desired_output_data)
+
+        result = big_countries(input_df)
+
+        self.assert_equal_dataframes(result, desired_df)
+
+    def no_matches(self):
+        input_data = [['Latveria', 'Europe', 600, 1000000],
+                      ['Wakanda', 'Africa', 28748, 25000, 1897900000],
+                      ['Duckburg', 'Calisota', 10000, 78115, 261200000000000000000]]
+        desired_df = output_dataframe([])
+
+        input_df = input_dataframe(input_data)
+
         result = big_countries(input_df)
         self.assert_equal_dataframes(result, desired_df)
 
