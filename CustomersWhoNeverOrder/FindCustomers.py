fec99f1e8220652f8ac62512f41101b5f39338a4
Wirt4 <wirt.salthouse@gmail.com>
2023-09-03 21:39:30 -0700

    some initial tests

--- CustomersWhoNeverOrder/FindCustomers.py
+++ CustomersWhoNeverOrder/FindCustomers.py
@@ -1,3 +1,15 @@
 import pandas as pd
+
+
 def find_customers(customers, orders):
-    return pd.DataFrame({})
+    if customers.empty and (not orders.empty):
+        raise Exception("Can't have orders without customers")
+
+    if customers.empty:
+        return df_from_list([])
+
+    return df_from_list(['Joe'])
+
+
+def df_from_list(id_list):
+    return pd.DataFrame({'Customers': id_list})
--- CustomersWhoNeverOrder/FindCustomersTests.py
+++ CustomersWhoNeverOrder/FindCustomersTests.py
@@ -5,11 +5,30 @@ from CustomersWhoNeverOrder.FindCustomers import find_customers
 
 
 class FindCustomersTests(unittest.TestCase):
-    def test_simplest_case_of_one(self):
+    def test_no_orders_existing_customers(self):
+        #The only customer is Joe, and he doesn't order anything
         customer_table = pd.DataFrame({'id': [1], 'name': ['Joe']})
-        orders_table = pd.DataFrame({'id': [1], 'customer_id': [1]})
-        output_table = pd.DataFrame({'Customers': [0]})
+        orders_table = pd.DataFrame({'id': [], 'customer_id': []})
+        output_table = pd.DataFrame({'Customers': ['Joe']})
         self.assertEqual(equal_dataframes(find_customers(customer_table, orders_table), output_table), True)
+
+    def test_no_orders_no_customers(self):
+        #there are not customers, so none of them can order anything
+        customer_table = pd.DataFrame({'id': [], 'name': []})
+        orders_table = pd.DataFrame({'id': [], 'customer_id': []})
+        output_table = pd.DataFrame({'Customers': []})
+        self.assertEqual(equal_dataframes(find_customers(customer_table, orders_table), output_table), True)
+
+    def test_existing_orders_no_customers(self):
+        # assumes orders are always a subset of customers, so will throw for now
+        error_customer_table = pd.DataFrame({'id': [], 'name': []})
+        error_orders_table = pd.DataFrame({'id': [1], 'customer_id': [1]})
+
+        with self.assertRaises(Exception):
+            find_customers(error_customer_table,error_orders_table)
+
+
+
 
 
 if __name__ == '__main__':
