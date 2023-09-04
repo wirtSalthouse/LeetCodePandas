103a893d557f5b7505c358117b633786ee9c414a
Wirt4 <wirt.salthouse@gmail.com>
2023-09-04 12:17:25 -0700

    added example case test

--- InvalidTweets/invalidTweetsTests.py
+++ InvalidTweets/invalidTweetsTests.py
@@ -12,6 +12,14 @@ class MyTestCase(unittest.TestCase):
         result = invalid_tweets(input_table)
         self.assertEqual(is_equal_dataframes(result, desired_output), True)
 
+    def test_supplied_test_case(self):
+        input_schema = ['tweet_id', 'content']
+        input_data = [[1, 'Vote for Biden'], [2, 'Let us make America great again!']]
+        input_table = pd.DataFrame(input_data, columns=input_schema)
+        desired_output = pd.DataFrame({'tweet_id': [2]})
+        result = invalid_tweets(input_table)
+        self.assertEqual(is_equal_dataframes(result, desired_output), True)
+
 
 if __name__ == '__main__':
     unittest.main()
