298374a6bdecef2b4efa7b3d7296e0a633146828
Wirt4 <wirt.salthouse@gmail.com>
2023-09-03 20:43:22 -0700

    clean up and DRY

--- RecyclableAndLowFatProducts/recyclableAndLowfatProducts.py
+++ RecyclableAndLowFatProducts/recyclableAndLowfatProducts.py
@@ -2,5 +2,11 @@ import pandas as pd
 
 
 def find_products(df):
-    ans = df.query('(low_fats == \'Y\') & (recyclable == \'Y\')')
-    return pd.DataFrame({'product_id': ans['product_id'].tolist()})
+    prod_id = 'product_id'
+
+    selection = df.query(f'{is_y("low_fats")} & {is_y("recyclable")}')
+    return pd.DataFrame({prod_id: selection[prod_id].tolist()})
+
+
+def is_y(key_string):
+    return f"({key_string} == 'Y')"
