a72c8d15ea7d3c2573b6b007af2bb26c9321a1af
Wirt4 <wirt.salthouse@gmail.com>
2023-09-04 11:57:03 -0700

    passes initial tests

--- ArticleViews1/articleViews.py
+++ ArticleViews1/articleViews.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
+
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame({'id':[]})
+    selection = views.query('(author_id == viewer_id)')
+    return pd.DataFrame({'id': remove_duplicates(selection['author_id'])})
+
+
+def remove_duplicates(author_ids):
+    return list(set(author_ids))
--- ArticleViews1/articleViews1Test.py
+++ ArticleViews1/articleViews1Test.py
@@ -6,10 +6,40 @@ from ArticleViews1.articleViews import article_views
 
 class ArticleViews1Tests(unittest.TestCase):
     def test_no_views(self):
-        no_views_table = pd.DataFrame({'article_id':[], 'author_id':[], 'viewer_id':[], 'view_date':[]})
-        desired_result = pd.DataFrame({'id':[]})
+        no_views_table = pd.DataFrame({'article_id': [], 'author_id': [], 'viewer_id': [], 'view_date': []})
+        desired_result = pd.DataFrame({'id': []})
         result = article_views(no_views_table)
-        self.assertEqual(is_equal_dataframes(result,desired_result), True)
+        self.assertEqual(is_equal_dataframes(result, desired_result), True)
+
+    def test_one_author_viewed_twice(self):
+        input_table = pd.DataFrame(
+            {'article_id': [3], 'author_id': [4], 'viewer_id': [4], 'view_date': ['2019-07-21']})
+        desired_result = pd.DataFrame({'id': [4]})
+        result = article_views(input_table)
+        self.assertEqual(is_equal_dataframes(result, desired_result), True)
+
+    def test_one_author_did_not_self_view(self):
+        input_table = pd.DataFrame(
+            {'article_id': [3, 3], 'author_id': [4, 4], 'viewer_id': [4, 5], 'view_date': ['2019-07-21', '2019-07-22']})
+        desired_result = pd.DataFrame({'id': [4]})
+        result = article_views(input_table)
+        self.assertEqual(is_equal_dataframes(result, desired_result), True)
+
+    def test_prompt_example(self):
+        input_schema = ['article_id', 'author_id', 'viewer_id', 'view_date']
+        input_data = [
+            [1, 3, 5, '2019-08-0'],
+            [1, 3, 6, '2019-08-02'],
+            [2, 7, 7, '2019-08-01'],
+            [2, 7, 6, '2019-08-02'],
+            [4, 7, 1, '2019-07-22'],
+            [3, 4, 4, '2019-07-21'],
+            [3, 4, 4, '2019-07-21']
+        ]
+        input_table = pd.DataFrame(input_data, columns=input_schema)
+        desired_result = pd.DataFrame({'id': [4, 7]})
+        result = article_views(input_table)
+        self.assertEqual(is_equal_dataframes(result, desired_result), True)
 
 
 if __name__ == '__main__':
