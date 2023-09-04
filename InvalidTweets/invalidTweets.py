0c006de7dafdbdcac6a84e82fb7066d0f481c9dc
Wirt4 <wirt.salthouse@gmail.com>
2023-09-04 12:32:49 -0700

    refactored tests, need more different input

--- InvalidTweets/invalidTweets.py
+++ InvalidTweets/invalidTweets.py
@@ -4,4 +4,5 @@ import pandas as pd
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
     if tweets.empty:
         return pd.DataFrame({'tweet_id': []})
-    return pd.DataFrame({'tweet_id': [2]})
+    filtered = tweets.query('content == \'Let us make America great again!\'')
+    return pd.DataFrame({'tweet_id': filtered['tweet_id'].values.tolist()})
--- InvalidTweets/invalidTweetsTests.py
+++ InvalidTweets/invalidTweetsTests.py
@@ -6,17 +6,27 @@ from TestUtils.testUtils import is_equal_dataframes
 
 
 class MyTestCase(unittest.TestCase):
+    def input_df(self, data):
+        return pd.DataFrame(data, columns=['tweet_id', 'content'])
+
+    def output_df(self, data):
+        return pd.DataFrame({'tweet_id': data})
+
     def test_empty_input(self):
-        input_table = pd.DataFrame({'tweet_id': [], 'content': []})
-        desired_output = pd.DataFrame({'tweet_id': []})
+        input_table = self.input_df([])
+        desired_output = self.output_df([])
         result = invalid_tweets(input_table)
         self.assertEqual(is_equal_dataframes(result, desired_output), True)
 
     def test_supplied_test_case(self):
-        input_schema = ['tweet_id', 'content']
-        input_data = [[1, 'Vote for Biden'], [2, 'Let us make America great again!']]
-        input_table = pd.DataFrame(input_data, columns=input_schema)
-        desired_output = pd.DataFrame({'tweet_id': [2]})
+        input_table = self.input_df([[1, 'Vote for Biden'], [2, 'Let us make America great again!']])
+        desired_output = self.output_df([2])
+        result = invalid_tweets(input_table)
+        self.assertEqual(is_equal_dataframes(result, desired_output), True)
+
+    def test_supplied_test_case_reordered(self):
+        input_table = self.input_df([[2, 'Vote for Biden'], [1, 'Let us make America great again!']])
+        desired_output = self.output_df([1])
         result = invalid_tweets(input_table)
         self.assertEqual(is_equal_dataframes(result, desired_output), True)
 
