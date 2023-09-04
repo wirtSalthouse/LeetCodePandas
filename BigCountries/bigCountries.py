8f96a73bf461a7d417c5e1473f3f6506f69bf205
Wirt4 <wirt.salthouse@gmail.com>
2023-09-03 19:24:56 -0700

    save passing test cases before more cleanup

--- BigCountries/bigCountries.py
+++ BigCountries/bigCountries.py
@@ -3,13 +3,17 @@ import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     """
-    returns a pandas DataFrame
+    returns a pandas DataFrame containing only "big countries",
+    table entries which have a area greater than 3,000,000 or population greater than 25,000,000
     :param world:
     :return:new dataframe with schema of name, population and area
     """
 
-    answer = world.query('(area >= 3000000) | (population >= 25000000)')
+    min_area = 3000000
+    min_population = 25000000
+
+    query_string = f'(area >= {min_area}) | (population >= {min_population})'
     output_schema = ['name', 'population', 'area']
-    answer = answer[output_schema]
-    print(answer)
-    return answer
+    output_data = world.query(query_string)[output_schema].values.tolist()
+    #whole new df to match indexing for test cases
+    return pd.DataFrame(output_data, columns=output_schema)
--- BigCountries/test.py
+++ BigCountries/test.py
@@ -2,15 +2,15 @@ import unittest
 import pandas as pd
 from bigCountries import big_countries
 
+def df_from_schema_and_data(scehma, data):
+    return pd.DataFrame(data,columns=scehma)
 
 def input_dataframe(data_list):
-    input_schema = ['name', 'continent', 'area', 'population', 'gdp']
-    return pd.DataFrame(data_list, columns=input_schema)
+    return df_from_schema_and_data(['name', 'continent', 'area', 'population', 'gdp'], data_list)
 
 
 def output_dataframe(data_list):
-    output_schema = ['name', 'population', 'area']
-    return pd.DataFrame(data_list, columns=output_schema)
+    return df_from_schema_and_data(['name', 'population', 'area'], data_list)
 
 
 class BigCountriesTest(unittest.TestCase):
@@ -22,12 +22,14 @@ class BigCountriesTest(unittest.TestCase):
                       ['Andorra', 'Europe', 468, 78115, 3712000000],
                       ['Angola', 'Africa', 1246700, 20609294, 100990000000]]
 
-        output_data = [['Afghanistan', 25500100, 652230],
+        desired_data = [['Afghanistan', 25500100, 652230],
                        ['Algeria', 37100000, 2381741]]
 
+        desired_result = output_dataframe(desired_data)
         input_df = input_dataframe(input_data)
-        output_df = output_dataframe(output_data)
-        self.assert_equal_dataframes(big_countries(input_df), output_df)
+        result = big_countries(input_df)
+
+        self.assert_equal_dataframes(result, desired_result)
 
     def test_case_2(self):
         input_data = [['Afghanistan', 'Asia', 652230, 25500100],
@@ -40,6 +42,18 @@ class BigCountriesTest(unittest.TestCase):
         input_df = input_dataframe(input_data)
         output_df = output_dataframe(output_data)
         self.assert_equal_dataframes(big_countries(input_df), output_df)
+
+    def single_entry_different_data(self):
+        input_data = [['Latveria', 'Europe', 600, 1000000],
+                      ['Wakanda', 'Africa', 28748, 25000000, 1897900000],
+                      ['Duckburg', 'Calisota', 10000, 78115, 261200000000000000000]]
+
+        output_data = [['Wakanda', 25000000, 28748]]
+
+        input_df = input_dataframe(input_data)
+        desired_df = output_dataframe(output_data)
+        result = big_countries(input_df)
+        self.assert_equal_dataframes(result, desired_df)
 
     def assert_equal_dataframes(self, df1, df2):
         self.assertEqual(df1.equals(df2), True)
