ffd66351bfc7d42c2c754183d26f90d63de40533
Wirt4 <wirt.salthouse@gmail.com>
2023-09-04 17:01:42 -0700

    cleaned up recyclableAndLowfatProducts.py

--- RecyclableAndLowFatProducts/findProductsTest.py
+++ RecyclableAndLowFatProducts/findProductsTest.py
@@ -7,52 +7,51 @@ from TestUtils.testUtils import is_equal_dataframes
 
 class MyTestCase(unittest.TestCase):
     def test_given_case(self):
-        input = pd.DataFrame({'product_id': [0, 1, 2, 3, 4], 'low_fats': ['Y', 'Y', 'N', 'Y', 'N'],
-                              'recyclable': ['N', 'Y', 'Y', 'Y', 'N']})
-        result = find_products(input)
+        input_table = pd.DataFrame({'product_id': [0, 1, 2, 3, 4], 'low_fats': ['Y', 'Y', 'N', 'Y', 'N'],
+                                    'recyclable': ['N', 'Y', 'Y', 'Y', 'N']})
+        result = find_products(input_table)
         expected = pd.DataFrame({'product_id': [1, 3]})
         self.assertEqual(is_equal_dataframes(result, expected), True)
 
     def test_different_data(self):
-        input = pd.DataFrame({'product_id': [0, 1, 2, 3, 4], 'low_fats': ['Y', 'N', 'N', 'Y', 'Y'],
-                              'recyclable': ['Y', 'Y', 'Y', 'N', 'Y']})
-        result = find_products(input)
+        input_table = pd.DataFrame({'product_id': [0, 1, 2, 3, 4], 'low_fats': ['Y', 'N', 'N', 'Y', 'Y'],
+                                    'recyclable': ['Y', 'Y', 'Y', 'N', 'Y']})
+        result = find_products(input_table)
         expected = pd.DataFrame({'product_id': [0, 4]})
         self.assertEqual(is_equal_dataframes(result, expected), True)
 
     def test_single_entry_true(self):
-        input = pd.DataFrame({'product_id': [0], 'low_fats': ['Y'],
-                              'recyclable': ['Y']})
-        result = find_products(input)
+        input_table = pd.DataFrame({'product_id': [0], 'low_fats': ['Y'],
+                                    'recyclable': ['Y']})
+        result = find_products(input_table)
         expected = pd.DataFrame({'product_id': [0]})
         self.assertEqual(is_equal_dataframes(result, expected), True)
 
     def test_single_entry_false_1(self):
-        input = pd.DataFrame({'product_id': [0], 'low_fats': ['N'],
-                              'recyclable': ['N']})
-        result = find_products(input)
-        expected = pd.DataFrame({'product_id': []})
-        self.assertEqual(is_equal_dataframes(result, expected), True)
+        input_table = pd.DataFrame({'product_id': [0], 'low_fats': ['N'],
+                                    'recyclable': ['N']})
+        result = find_products(input_table)
+        self.assertEqual(result.empty, True)
 
     def test_single_entry_false_2(self):
-        input = pd.DataFrame({'product_id': [0], 'low_fats': ['N'],
-                              'recyclable': ['Y']})
-        result = find_products(input)
-        expected = pd.DataFrame({'product_id': []})
-        self.assertEqual(is_equal_dataframes(result, expected), True)
+        input_table = pd.DataFrame({'product_id': [0], 'low_fats': ['N'],
+                                    'recyclable': ['Y']})
+        result = find_products(input_table)
+        self.assertEqual(result.empty, True)
 
     def test_single_entry_false_3(self):
-        input = pd.DataFrame({'product_id': [0], 'low_fats': ['Y'],
+        input_table = pd.DataFrame({'product_id': [0], 'low_fats': ['Y'],
                               'recyclable': ['N']})
-        result = find_products(input)
-        expected = pd.DataFrame({'product_id': []})
-        self.assertEqual(is_equal_dataframes(result, expected), True)
+        result = find_products(input_table)
+        self.assertEqual(result.empty, True)
 
     def test_single_entry_true(self):
-        input = pd.DataFrame({'product_id': [0], 'low_fats': ['Y'],
+        input_table = pd.DataFrame({'product_id': [0], 'low_fats': ['Y'],
                               'recyclable': ['Y']})
-        result = find_products(input)
+        result = find_products(input_table)
         expected = pd.DataFrame({'product_id': [0]})
         self.assertEqual(is_equal_dataframes(result, expected), True)
+
+
 if __name__ == '__main__':
     unittest.main()
--- RecyclableAndLowFatProducts/recyclableAndLowfatProducts.py
+++ RecyclableAndLowFatProducts/recyclableAndLowfatProducts.py
@@ -5,6 +5,7 @@ def find_products(df):
     prod_id = 'product_id'
 
     selection = df.query(f'{is_y("low_fats")} & {is_y("recyclable")}')
+    return selection.drop(['low_fats', 'recyclable'], axis=1).reset_index(drop=True)
     return pd.DataFrame({prod_id: selection[prod_id].tolist()})
 
 
