232fd9383d7653293a5dfc1a1e77dd32d8496167
Wirt4 <wirt.salthouse@gmail.com>
2023-09-08 19:16:46 -0700

    basic tests

--- find_patients/find_patients.py
+++ find_patients/find_patients.py
@@ -1,5 +1,19 @@
 import pandas as pd
 
+
 # define function here:
 def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
-    return patients
+    row = patients.iloc[0]
+    condition_string = row['conditions']
+    if has_prefix(condition_string, 'DIAB'):
+        return patients
+
+    return pd.DataFrame()
+
+
+def has_prefix(conditions: str, prefix: str) -> bool:
+    cond_list = conditions.split(' ')
+    for c in cond_list:
+        if c.startswith(prefix):
+            return True
+    return False
--- find_patients/find_patients_tests.py
+++ find_patients/find_patients_tests.py
@@ -11,5 +11,15 @@ class find_patientsTests(unittest.TestCase):
 		filtered_table = find_patients(input_table)
 		self.assertEqual(is_equal_dataframes(input_table, filtered_table), True)
 
+	def test_single_entry_no_matches(self):
+		input_table = pd.DataFrame([['5', 'Tim', 'ACNE']], columns=['patient_id', 'patient_name', 'conditions'])
+		filtered_table = find_patients(input_table)
+		self.assertEqual(filtered_table.empty,  True)
+
+	def test_single_entry_no_change_comma_data(self):
+		input_table = pd.DataFrame([['3', 'Don', 'COUGH DIAB007']], columns=['patient_id', 'patient_name', 'conditions'])
+		filtered_table = find_patients(input_table)
+		self.assertEqual(is_equal_dataframes(input_table, filtered_table), True)
+
 if __name__ == '__main__':
 	unittest.main()
