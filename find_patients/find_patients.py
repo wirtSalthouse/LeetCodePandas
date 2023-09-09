478ca5977124b5243d200f462ff9f724fd2c7fd1
Wirt4 <wirt.salthouse@gmail.com>
2023-09-08 19:57:57 -0700

    ready to take it live

--- find_patients/find_patients.py
+++ find_patients/find_patients.py
@@ -1,16 +1,9 @@
 import pandas as pd
 
 
-# define function here:
 def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
-    if patients.size == 3:
-        row = patients.iloc[0]
-        condition_string = row['conditions']
-        if has_prefix(condition_string, 'DIAB1'):
-            return patients
-        return pd.DataFrame()
+    return patients[patients.apply(lambda x: has_prefix(x.conditions, 'DIAB1'), axis=1)].reset_index(drop=True)
 
-    return pd.DataFrame([[3, 'Bob', 'DIAB100 MYOP'], [4, 'George', 'ACNE DIAB100']], columns=['patient_id', 'patient_name', 'conditions'])
 
 def has_prefix(conditions: str, prefix: str) -> bool:
     cond_list = conditions.split(' ')
