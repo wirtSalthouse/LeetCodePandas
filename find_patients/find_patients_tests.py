87e3fa32d35306ce08eb63f60268b9cb5f5e5f1a
Wirt4 <wirt.salthouse@gmail.com>
2023-09-08 18:54:21 -0700

    initate with README.md

---
+++ find_patients/find_patients_tests.py
@@ -0,0 +1,13 @@
+import unittest
+import pandas as pd
+from TestUtils.testUtils import is_equal_dataframes
+
+
+# remember to import the function to test
+
+class find_patientsTests(unittest.TestCase):
+	def test_something(self):
+		self.assertEqual(True, False)
+
+if __name__ == '__main__':
+	unittest.main()
