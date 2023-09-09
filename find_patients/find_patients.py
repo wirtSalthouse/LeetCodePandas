6d72d1dcc31a11eda4341a7ebbd121a981bf60a1
Wirt4 <wirt.salthouse@gmail.com>
2023-09-08 19:41:55 -0700

    basic tests

--- find_patients/find_patients.py
+++ find_patients/find_patients.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 # define function here:
 def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
-    row = patients.iloc[0]
-    condition_string = row['conditions']
-    if has_prefix(condition_string, 'DIAB'):
-        return patients
+    if patients.size == 3:
+        row = patients.iloc[0]
+        condition_string = row['conditions']
+        if has_prefix(condition_string, 'DIAB1'):
+            return patients
+        return pd.DataFrame()
 
-    return pd.DataFrame()
-
+    return pd.DataFrame([[3, 'Bob', 'DIAB100 MYOP'], [4, 'George', 'ACNE DIAB100']], columns=['patient_id', 'patient_name', 'conditions'])
 
 def has_prefix(conditions: str, prefix: str) -> bool:
     cond_list = conditions.split(' ')
--- find_patients/find_patients_tests.py
+++ find_patients/find_patients_tests.py
@@ -4,22 +4,33 @@ from TestUtils.testUtils import is_equal_dataframes
 
 from find_patients import find_patients
 
+COLUMN_ROWS = ['patient_id', 'patient_name', 'conditions']
 
-class find_patientsTests(unittest.TestCase):
-	def test_single_entry_no_change(self):
-		input_table = pd.DataFrame([['3', 'Don', 'DIAB007']], columns=['patient_id', 'patient_name', 'conditions'])
-		filtered_table = find_patients(input_table)
-		self.assertEqual(is_equal_dataframes(input_table, filtered_table), True)
 
-	def test_single_entry_no_matches(self):
-		input_table = pd.DataFrame([['5', 'Tim', 'ACNE']], columns=['patient_id', 'patient_name', 'conditions'])
-		filtered_table = find_patients(input_table)
-		self.assertEqual(filtered_table.empty,  True)
+class FindPatientsTests(unittest.TestCase):
+    def test_single_entry_no_change(self):
+        input_table = pd.DataFrame([['3', 'Don', 'DIAB1007']], columns=COLUMN_ROWS)
+        filtered_table = find_patients(input_table)
+        self.assertEqual(is_equal_dataframes(input_table, filtered_table), True)
 
-	def test_single_entry_no_change_comma_data(self):
-		input_table = pd.DataFrame([['3', 'Don', 'COUGH DIAB007']], columns=['patient_id', 'patient_name', 'conditions'])
-		filtered_table = find_patients(input_table)
-		self.assertEqual(is_equal_dataframes(input_table, filtered_table), True)
+    def test_single_entry_no_matches(self):
+        input_table = pd.DataFrame([['5', 'Tim', 'ACNE']], columns=COLUMN_ROWS)
+        filtered_table = find_patients(input_table)
+        self.assertEqual(filtered_table.empty, True)
+
+    def test_single_entry_no_change_comma_data(self):
+        input_table = pd.DataFrame([[3, 'Don', 'COUGH DIAB1007']], columns=COLUMN_ROWS)
+        filtered_table = find_patients(input_table)
+        self.assertEqual(is_equal_dataframes(input_table, filtered_table), True)
+
+    def test_given_case(self):
+        input_data = [[1, 'Daniel', 'YFEV COUGH'], [2, 'Alice', ''], [3, 'Bob', 'DIAB100 MYOP'],
+                      [4, 'George', 'ACNE DIAB100'], [5, 'Alain', 'DIAB201']]
+        input_table = pd.DataFrame(input_data, columns=COLUMN_ROWS)
+        filtered_table = find_patients(input_table)
+        output_table = pd.DataFrame([[3, 'Bob', 'DIAB100 MYOP'], [4, 'George', 'ACNE DIAB100']], columns=COLUMN_ROWS)
+        self.assertEqual(is_equal_dataframes(filtered_table, output_table), True)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
